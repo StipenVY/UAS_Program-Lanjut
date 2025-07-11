f09981d69b4cad9039073608e6c10baba035e911
StipenVY <sandal123batang@gmail.com>
2025-07-11 22:14:11 +0700

    Add files via upload

---
+++ uas_crud.sql
@@ -0,0 +1,57 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 11 Jul 2025 pada 17.09
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `uas_crud`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `biodata`
+--
+
+CREATE TABLE `biodata` (
+  `NIK` varchar(50) NOT NULL,
+  `NAMA` varchar(50) NOT NULL,
+  `TELEPHONE` varchar(13) NOT NULL,
+  `ALAMAT` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `biodata`
+--
+
+INSERT INTO `biodata` (`NIK`, `NAMA`, `TELEPHONE`, `ALAMAT`) VALUES
+('24083000044', 'Steven', '085123456789', 'Jl.Bululawang');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `biodata`
+--
+ALTER TABLE `biodata`
+  ADD PRIMARY KEY (`NIK`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
